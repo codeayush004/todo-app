265d17681d9e0cf4408c0e2dfe35a62de86a066d
Ayush Singh <ayush.singh@sigmoidanalytics.com>
2025-10-23 18:57:01 +0530

    update4.0

--- frontend.py
+++ frontend.py
@@ -4,7 +4,7 @@ import requests
 
 st.set_page_config(page_title="Todo App", page_icon="📝", layout="centered")
 
-st.title("📝 Todo App (AKS&GIps)")
+st.title("📝 Todo App (AKps)")
 st.caption("A minimal Streamlit UI connected to your FastAPI backend")
 
 # 🧠 Replace this with your backend’s public IP if running remotely
